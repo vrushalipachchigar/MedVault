99fc12f1dbb5771003e5dcb5f279ac7e5187e1b2
[vrushalipachchigar] <[vrushalipachchigar@gmail.com]>
2023-07-19 23:43:05 -0400

    Implemented Backend

---
+++ Medicines/app/config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    url: "mongodb+srv://vrushali:vrushalipachchigar@dressstore.m3hhp97.mongodb.net/?retryWrites=true&w=majority"
+
+};
---
+++ Medicines/app/routes/medicine.routes.js
@@ -0,0 +1,30 @@
+module.exports = app => {
+
+    const medicines = require("../controllers/medicine.controller.js");
+
+    var router = require("express").Router();
+
+    // Create a new Medicine
+    router.post("/", medicines.create);
+
+    // Retrieve all Medicines
+    router.get("/", medicines.findAll);
+
+    // Retrieve all published Medicines
+    router.get("/published", medicines.findAllPublished);
+
+    // Retrieve a single Medicine with id
+    router.get("/:id", medicines.findOne);
+
+    // Update a Medicine with id
+    router.put("/:id", medicines.update);
+
+    // Delete a Medicine with id
+    router.delete("/:id", medicines.delete);
+
+    // Delete all Medicines
+    router.delete("/", medicines.deleteAll);
+
+    app.use('/api/medicines', router);
+
+};
